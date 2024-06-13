fc65735417339c29bf5d14f4ab8607334cfcbb58
keith kroculick <kkroculick@gmail.com>
2024-06-13 10:13:04 -0700

    next commit

--- src/main/java/org/myorg/quickstart/DataStreamJob.java
+++ src/main/java/org/myorg/quickstart/DataStreamJob.java
@@ -19,6 +19,7 @@
 package org.myorg.quickstart;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.jdbc.catalog.JdbcCatalog;
@@ -374,10 +375,43 @@ public class DataStreamJob {
         //new TopicPartition("topic-b", 5)));  // Partition 5 of topic "topic-b"
         // KafkaSource.builder().setPartitions(partitionSet);
         // deprecated - FlinkKafkaConsumer is deprecated
+
         DataStream<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-       //DataStream<Tuple2<String, Integer>> processedStream = stream
+        //DataStream<Tuple2<String, Integer>> processedStream = stream
         //stream.print();
-      //  env.execute();
+        //env.execute();
+      
+      
+      
+      /* DataStream<Tuple2<String, Integer>> wordCounts = env.fromElements(
+                new Tuple2<String, Integer>("hello", 1),
+                new Tuple2<String, Integer>("world", 2));
+
+        wordCounts.map(new MapFunction<Tuple2<String, Integer>, Integer>() {
+            @Override
+            public Integer map(Tuple2<String, Integer> value) throws Exception {
+                return value.f1;
+            }
+        });
+
+        wordCounts.keyBy(value -> value.f0);
+        */
+
+        DataStream<String> control = env
+        .fromElements("DROP", "IGNORE")
+        .keyBy(x -> x);
+
+    DataStream<String> streamOfWords = env
+        .fromElements("Apache", "DROP", "Flink", "IGNORE")
+        .keyBy(x -> x);
+  
+    control
+        .connect(streamOfWords)
+        .flatMap(new ControlFunction())
+        .print();
+
+    env.execute();
+
 
     }
 }
